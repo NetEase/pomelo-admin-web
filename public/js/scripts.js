aadc9ccdc9447b5015bb79ac0d150e4ac3f6ba73
fantasyni <fantasyni@163.com>
2013-04-11 11:36:14 +0800

    fix layout bug

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,15 +1,14 @@
+Ext.onReady(function() {
 
-Ext.onReady(function(){
+	Ext.BLANK_IMAGE_URL = '../ext-4.0.7-gpl/resources/themes/images/default/tree/s.gif';
 
-	Ext.BLANK_IMAGE_URL ='../ext-4.0.7-gpl/resources/themes/images/default/tree/s.gif'; 
-    
-    //server comboBox
-    var serverStore = Ext.create('Ext.data.Store', {
+	//server comboBox
+	var serverStore = Ext.create('Ext.data.Store', {
 		fields: ['name', 'serverId']
 	});
 
-    var serverCom = Ext.create('Ext.form.ComboBox', {
-		id:'serverComId', 
+	var serverCom = Ext.create('Ext.form.ComboBox', {
+		id: 'serverComId',
 		fieldLabel: 'On Server',
 		labelWidth: 60,
 		store: serverStore,
@@ -18,22 +17,22 @@ Ext.onReady(function(){
 		valueField: 'name'
 	});
 
-    //script comboBox
-    var scriptStore = Ext.create('Ext.data.Store', {
+	//script comboBox
+	var scriptStore = Ext.create('Ext.data.Store', {
 		fields: ['name', 'script']
 	});
 
-    var scriptCom = Ext.create('Ext.form.ComboBox', {
-		id:'scriptComId',
+	var scriptCom = Ext.create('Ext.form.ComboBox', {
+		id: 'scriptComId',
 		fieldLabel: 'Script List',
 		labelWidth: 80,
 		store: scriptStore,
 		queryMode: 'local',
 		displayField: 'script',
 		valueField: 'name',
-		listeners:{
-			select:{
-				fn:function(){
+		listeners: {
+			select: {
+				fn: function() {
 					var value = scriptCom.getValue();
 					get(value);
 				}
@@ -46,38 +45,45 @@ Ext.onReady(function(){
 		autoScroll: true,
 		autoShow: true,
 		renderTo: Ext.getBody(),
-		items:[{
+		region: 'center',
+		items: [{
 			layout: 'column',
 			border: false,
 			anchor: '95%',
-			items: [{xtype: 'label', text: 'Run Script:', columnWidth: .99}, serverCom, scriptCom]
+			items: [{
+				xtype: 'label',
+				text: 'Run Script:',
+				columnWidth: .99
+			},
+			serverCom, scriptCom]
 		}, {
 			xtype: 'textareafield',
 			height: 150,
+			//region: 'center',
 			id: 'scriptAreaId',
 			anchor: '95%'
 		}, {
 			layout: 'column',
 			anchor: '95%',
 			border: false,
-			items:[{
+			items: [{
 				// colspan: 2
-				xtype:'button',
+				xtype: 'button',
 				text: 'Run',
 				handler: run,
 				width: 150,
 				margin: '10 0 10 900'
-			},{
+			}, {
 				// colspan: 2
-				xtype:'button',
-				text:'Save',
+				xtype: 'button',
+				text: 'Save',
 				handler: saveForm,
-				width:150,
+				width: 150,
 				margin: '10 0 10 100'
 			}]
 		}, {
-			xtype:'label',
-			text:'Result:'
+			xtype: 'label',
+			text: 'Result:'
 			// height:20
 		}, {
 			xtype: 'textareafield',
@@ -104,60 +110,69 @@ var saveForm = function() {
 		// renderTo: Ext.getBody(),
 		anchor: '100%',
 		fieldDefaults: {
-            msgTarget: 'side',
-            labelWidth: 50
-        },
+			msgTarget: 'side',
+			labelWidth: 50
+		},
 		items: [{
-			xtype:'textfield',
-			id:'scriptNameId',
-			fieldLabel:'name',
-			name:'scriptName',
-			allowBlank:false,
-			width:250,
-			value:Ext.getCmp('scriptComId').getValue()
+			xtype: 'textfield',
+			id: 'scriptNameId',
+			fieldLabel: 'name',
+			name: 'scriptName',
+			allowBlank: false,
+			width: 250,
+			value: Ext.getCmp('scriptComId').getValue()
 		}],
 		buttons: [{
-            text: 'Save',
-            handler: save
-        }, {
-            text: 'Cancel',
-            handler: cancel
-        }]
+			text: 'Save',
+			handler: save
+		}, {
+			text: 'Cancel',
+			handler: cancel
+		}]
 	});
 
-	var win=Ext.create('Ext.window.Window',{
-		id:'saveWinId',
-		title:'saveScript',
-		height:100,
-		width:320,
-		layout:'fit',
-		anchor:'100%',
-		items:[saveForm]
+	var win = Ext.create('Ext.window.Window', {
+		id: 'saveWinId',
+		title: 'saveScript',
+		height: 100,
+		width: 320,
+		layout: 'fit',
+		anchor: '100%',
+		items: [saveForm]
 	});
 
 	win.show();
 };
 
-var cancel = function(){
-    Ext.getCmp('saveWinId').close();
+var cancel = function() {
+	Ext.getCmp('saveWinId').close();
 };
 
 var list = function() {
-	window.parent.client.request('scripts', {command: 'list'}, function(err, msg) {
-		if(err) {
+	window.parent.client.request('scripts', {
+		command: 'list'
+	}, function(err, msg) {
+		if (err) {
 			alert(err);
 			return;
 		}
-
-		var servers = [], scripts = [], i, l, item;
-		for(i=0, l=msg.servers.length; i<l; i++) {
+		var servers = [],
+			scripts = [],
+			i, l, item;
+		for (i = 0, l = msg.servers.length; i < l; i++) {
 			item = msg.servers[i];
-			servers.push({name: item, serverId: item});
+			servers.push({
+				name: item,
+				serverId: item
+			});
 		}
 
-		for(i=0, l=msg.scripts.length; i<l; i++) {
+		for (i = 0, l = msg.scripts.length; i < l; i++) {
 			item = msg.scripts[i];
-			scripts.push({name: item, script: item});
+			scripts.push({
+				name: item,
+				script: item
+			});
 		}
 
 		servers.sort(function(o1, o2) {
@@ -174,17 +189,21 @@ var list = function() {
 };
 
 //run the script
-var run = function(){
+var run = function() {
 	var scriptJs = Ext.getCmp('scriptAreaId').getValue();
 	var serverId = Ext.getCmp('serverComId').getValue();
 
-	if(!serverId) {
+	if (!serverId) {
 		alert('serverId is required!');
 		return;
 	}
 
-	window.parent.client.request('scripts', {command: 'run', serverId: serverId, script: scriptJs}, function(err, msg) {
-		if(err) {
+	window.parent.client.request('scripts', {
+		command: 'run',
+		serverId: serverId,
+		script: scriptJs
+	}, function(err, msg) {
+		if (err) {
 			alert(err);
 			return;
 		}
@@ -192,9 +211,12 @@ var run = function(){
 	});
 };
 
-var get = function(filename){
-	window.parent.client.request('scripts', {command: 'get', filename: filename}, function(err, msg) {
-		if(err) {
+var get = function(filename) {
+	window.parent.client.request('scripts', {
+		command: 'get',
+		filename: filename
+	}, function(err, msg) {
+		if (err) {
 			alert(err);
 			return;
 		}
@@ -202,22 +224,25 @@ var get = function(filename){
 	});
 };
 
-var save = function(){
-	var filename=Ext.getCmp('scriptNameId').getValue();
-	if(!filename.match(/\.js$/)){
+var save = function() {
+	var filename = Ext.getCmp('scriptNameId').getValue();
+	if (!filename.match(/\.js$/)) {
 		alert('the filename is required!');
 		return;
 	}
 
-	var data=Ext.getCmp('scriptAreaId').getValue();
+	var data = Ext.getCmp('scriptAreaId').getValue();
 
-	window.parent.client.request('scripts', {command: 'save', filename: filename, body: data}, function(err, msg) {
-		if(err) {
+	window.parent.client.request('scripts', {
+		command: 'save',
+		filename: filename,
+		body: data
+	}, function(err, msg) {
+		if (err) {
 			alert(err);
 			return;
 		}
 		alert('save success!');
-		data=Ext.getCmp('scriptAreaId').setValue('');
+		data = Ext.getCmp('scriptAreaId').setValue('');
 	});
 };
-
