9550c1f052fac703843699762347719e09db9b1b
xiaodong <875697126@qq.com>
2017-02-21 10:49:34 +0800

    update express version
    update express useage

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 var express = require('express');
+var path = require('path');
+var fs = require('fs');
+var logger = require('morgan');
+var compression = require('compression');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
 var config = require('./config/admin');
 
 var app = express();
@@ -7,31 +14,17 @@ var app = express();
 var pub = __dirname + '/public';
 var view = __dirname + '/views';
 
-app.configure(function() {
-	app.set('view engine', 'html');
-	app.set('views', view);
-	app.engine('.html', require('ejs').__express);
+app.use(compression());
 
-	app.use(express.methodOverride());
-	app.use(express.bodyParser());
-	app.set('basepath', __dirname);
-});
+app.use(logger(':method :url :response-time ms'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ type: '*/*' }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.configure('development', function() {
-	app.use(express.static(pub));
-	app.use(express.errorHandler({
-		dumpExceptions: true,
-		showStack: true
-	}));
-});
-
-app.configure('production', function() {
-	var oneYear = 31557600000;
-	app.use(express.static(pub, {
-		maxAge: oneYear
-	}));
-	app.use(express.errorHandler());
-});
+app.set('view engine', 'html');
+app.set('views', view);
+app.engine('.html', require('ejs').__express);
 
 app.on('error', function(err) {
 	console.error('app on error:' + err.stack);
